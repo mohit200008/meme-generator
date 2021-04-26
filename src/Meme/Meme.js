203d860b41533828da91d8efe8e1604a1ff14b96
mohit <mohitklamba@gmail.com>
2021-04-26 17:27:20 +0530

    meme generator url made

--- src/Meme/Meme.js
+++ src/Meme/Meme.js
@@ -1,11 +1,49 @@
 import React, { useEffect, useState } from 'react';
 import './styles.modules.css';
+import { useHistory } from 'react-router-dom';
 
 export const Meme= () => {
 
     const [memes, setMemes]= useState([]);
     const [memeIndex,setMemeIndex]=useState(0);
     const [captions, setCaptions]= useState([]);
+
+    const history = useHistory();
+
+    const updateCaption=(e,index) => {
+        const text= e.target.value || '';
+        setCaptions(
+            captions.map((c,i)=> {
+                if(index===i){
+                    return text;
+
+                } else {
+                    return c;
+
+                }
+            })
+        )
+    };
+
+    const generateMeme = () => {
+        const currentMeme= memes[memeIndex];
+        const formData= new FormData();
+
+        formData.append('username','MohitLamba1');
+        formData.append('password','Abcde12345');
+        formData.append('template_id',currentMeme.id);
+        captions.forEach((c,index)=> formData.append(`boxes[${index}][text]`,c));
+
+        fetch('https://api.imgflip.com/caption_image',{
+            method: 'POST',
+            body: formData
+        }).then(res=> {
+            res.json().then(res=> {
+                history.push(`/generated?url=${res.data.url}`);
+            })
+        })
+
+    };
 
 
     const shuffleMemes = (array) => {
@@ -35,20 +73,17 @@ export const Meme= () => {
 
     },[memeIndex,memes]);
 
-    useEffect(()=> {
-        console.log(captions);
-    },[captions]);
-
+ 
 
 
     return (
         memes.length ? 
        <div className= "container">
-         <button onClick={()=> console.log('Generate!')}  className= "generate">Generate</button> 
+         <button onClick={generateMeme}  className= "generate">Generate</button> 
          <button onClick={()=> setMemeIndex(memeIndex+1)}  className= "skip">Skip</button> 
          {
              captions.map((c,index)=> (
-                 <input key={index} />
+                 <input onChange={(e)=> updateCaption(e,index)} key={index} />
              ))
          }
          <img src={memes[memeIndex].url}/> 
